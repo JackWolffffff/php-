10775ec192ebed6a7560801b8794e94f483175bb
JackWolffffff <767192715@qq.com>
2017-01-22 23:22:13 +0800

    20170122
    php多维数组、php日期和时间

---
+++ php多维数组.php
@@ -0,0 +1,12 @@
+<?php 
+	$cars = array(array("Volvo", 22, 18),
+		array("BMW", 15, 13),
+		array("Saab", 5, 2),
+		array("Land Rover", 17, 15));
+	for ($i=0; $i < count($cars); $i++) { 
+		echo "<br>";
+		for ($j=0; $j < count($cars[$i]); $j++) { 
+			echo $cars[$i][$j]." ";
+		}
+	}
+?>
---
+++ php日期和时间.php
@@ -0,0 +1,9 @@
+<?php 
+	echo "时间戳<br>";
+	echo "Today is " . date("Y/m/d") . "<br>";
+	echo "Today is " . date("Y.m.d") . "<br>";
+	echo "Today is " . date("Y-m-d") . "<br>";
+	echo "Today is " . date("l") . "<br>";
+	echo "2010-" . date("Y");
+	echo "现在时间是 " . date("h:i:sa");
+?>
