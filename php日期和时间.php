31717a1cfdacc14b18ac2df90b56677ed54c0208
rongjun <767192715@qq.com>
2017-01-23 18:12:16 +0800

    20170123
    20170123

--- php日期和时间.php
+++ php日期和时间.php
@@ -6,4 +6,16 @@
 	echo "Today is " . date("l") . "<br>";
 	echo "2010-" . date("Y");
 	echo "现在时间是 " . date("h:i:sa");
+	echo "<br>使用mktime创建日期<br>";
+	$d = mktime(9, 12, 31, 6, 10, 2107);
+	echo "创建的日期是: " . date("Y-m-d h:i:sa", $d);
+	echo "<br>使用strtotime创建日期<br>";
+	$d2 = strtotime("11:32am April 15 2015");
+	echo "创建的日期是: " . date("Y-m-d h:i:sa", $d2);
+	$d3 = strtotime("tomorrow");
+	echo "<br>tomorrow: " . date("Y-m-d h:i:sa", $d3);
+	$d4 = strtotime("next Monday");
+	echo "<br>next Monday: " . date("Y-m-d h:i:sa", $d4);
+	$d5 = strtotime("+3 Months");
+	echo "<br>" . date("Y-m-d h:i:sa", $d5);
 ?>
